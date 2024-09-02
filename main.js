db36dd618873080c0910bd9dbc7a07142f2dac01
Gustavo-Mendess <gustavormendess@gmail.com>
2024-09-02 09:47:48 -0400

    Adicionar funções de alta ordem

--- main.js
+++ main.js
@@ -1,5 +1,42 @@
 // High order functions(Reduce,sort,map,filter,foreach)
 
+const carros = [
+  {
+    id: "1",
+    marca: "Ferrari",
+    preco: 10000,
+  },
+  {
+    id: "2",
+    marca: "Mercedes",
+    preco: 30000,
+  },
+  {
+    id: "3",
+    marca: "BMW",
+    preco: 450000,
+  },
+];
+
+console.log(
+  carros.reduce((acc, car) => {
+    return acc + car.preco;
+  }, 0)
+);
+
+const letras = ["e", "f", "g", "a", "d", "c", "b"];
+console.log(letras.sort());
+const numerosDesordenados = [2, 5, 3, 1, 4, 6];
+console.log(
+  numerosDesordenados.sort((a, b) => {
+    return a - b;
+  })
+);
+console.log(carros.map((car) => car.marca));
+console.log(carros.filter((car) => car.preco > 10000));
+carros.forEach((car) => {
+  console.log(car.preco.toFixed(2));
+});
 // Array
 
 // Objetos
